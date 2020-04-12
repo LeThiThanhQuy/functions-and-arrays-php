2abe64d4ea33f0f1ab86e58d1b83dc24e8896127
Thanh Quy <thanhquylethi@gmail.com>
2020-04-12 21:58:01 +0700

    Add Merging array php file

---
+++ merging-array/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Merging array</title>
+</head>
+<body>
+<?php
+$arr1 = [ 1,8,9,23,7,6,3];
+$arr2 = [ 6,12,98,102,7,4];
+$arr3 = [];
+
+$arr3 = array_merge($arr1,$arr2);//Merging array
+echo '<pre>';
+print_r($arr3)	;
+echo '<pre>';
+?>
+</body>
+</html>
